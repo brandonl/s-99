cc72f60320e963a5ceb85fec69b18e8d1bbefd8d
brandonl <brandonluth@gmail.com>
2013-03-09 13:58:44 -0800

    first

---
+++ .worksheet/src/ListsModule.scala
@@ -0,0 +1,4 @@
+object ListsModule
+{import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(36); 
+	println( "h" )}
+}
---
+++ src/ListsModule.sc
@@ -0,0 +1,4 @@
+object ListsModule
+{
+	println( "h" )
+}
